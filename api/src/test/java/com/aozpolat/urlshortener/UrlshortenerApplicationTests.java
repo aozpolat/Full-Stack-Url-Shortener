778f2dc7016b9b9944f6a574359c89a0aebebdb0
aozpolat <akdrozpolat@gmail.com>
2022-09-18 08:20:28 +0300

    create main structure

---
+++ api/src/test/java/com/aozpolat/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aozpolat.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
