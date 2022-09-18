b2a47efb76fbc26c37255f28289ec72db63e22ca
aozpolat <akdrozpolat@gmail.com>
2022-09-18 11:35:56 +0300

    add style for main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,9 @@ const App = () => {
       <header>Url Shortener</header>
       <div className="input-container">
         <label htmlFor="url">Type a url</label>
+
         <input name="url" type="text" />
+        <button>Shorten</button>
       </div>
     </div>
   );
